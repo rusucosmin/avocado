f027b75b5a495525abf2410bff5ce2009cf996f0
Cosmin <cr.rusucosmin@gmail.com>
2017-10-27 00:06:46 +0300

    Added users

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'authenticate', to: 'authentication#authenticate'
 end
